4d0126b97415dd3f9572a1d29310389666c99da3
iancotosoto <iancotosoto@gmail.com>
2024-06-15 01:36:36 -0600

    More functions for webscrapping
    To transform the data

--- app/data/utils/converter.py
+++ app/data/utils/converter.py
@@ -1,2 +1,11 @@
+import app.data.utils.validator as validator
+
+# Functions
+# Countries
 def convert_continent(continent:str):
     return "America" if continent == "Americas" else continent
+
+# Astronomers
+def convert_year(year:str):
+    year = year.strip()
+    return int(year) if validator.validate_year(year) else -1
--- app/data/utils/validator.py
+++ app/data/utils/validator.py
@@ -1,7 +1,17 @@
 import re
 
+# Functions
+# Country
 def validate_code_country(code_country: str) -> bool:
     """
-    Validate the code of a country
+    Validate the code of a country (ISO 3166: 3 letters)
     """
     return re.match(r"^[A-Z]{3}$", code_country) != None
+
+# Astronomers
+def validate_year(year: str) -> bool:
+    """
+    Validate the year of birth or death of an astronomer (1 to 4 digits)
+    Only after christ and in normal conditions (digits)
+    """
+    return re.match(r"^\d{1,4}$", year) != None
